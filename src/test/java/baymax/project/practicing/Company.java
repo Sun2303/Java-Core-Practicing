f95a7c228df98e9669a26d29ce89bc3b726c4450
Sun Automation <sun.libra95@gmail.com>
2023-07-29 19:50:05 +0700

    Searching for Lombok - Example

---
+++ src/test/java/baymax/project/practicing/Company.java
@@ -0,0 +1,24 @@
+package baymax.project.practicing;
+
+import lombok.Data;
+
+@Data
+public class Company {
+    private String companyName, country, service;
+    private int yearOfBuild;
+
+    public static void main(String[] args) {
+        Company company = new Company();
+        company.setCompanyName("Sun SCD Limited");
+        company.setCountry("Vietnam");
+        company.setYearOfBuild(2000);
+        company.setService("Transportation");
+
+        System.out.println("Company name: " + company.getCompanyName());
+        System.out.println("Country: " + company.getCountry());
+        System.out.println("Year of build: " + company.getYearOfBuild());
+        System.out.println("Service: " + company.getService());
+        System.out.println(company);
+
+    }
+}
