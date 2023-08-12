9a94cf92622b46288dbd474062d5d9db68ada517
Sun Automation <sun.libra95@gmail.com>
2023-08-12 23:04:48 +0700

    Optimize TCs by using @Before

--- src/test/java/baymax/project/product_management/CoffeeShopManagementTest.java
+++ src/test/java/baymax/project/product_management/CoffeeShopManagementTest.java
@@ -12,6 +12,17 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CoffeeShopManagementTest {
+    private Employees employee01, employee02, employee03, employee04, employee05;
+    private List<Employees> employeeList;
+    @Before
+    public void setUp() {
+        employee01 = new Employees("Sun Beo", "Supervisor", 02, 35);
+        employee02 = new Employees("Tran Tuan", "Waiter", 01, 20);
+        employee03 = new Employees("Nguyen Minh", "Waiter", 01, 20);
+        employee04 = new Employees("Le Hieu", "Cashier", 03, 35);
+        employee05 = new Employees("Le Hoang", "Warehouse Manager", 03, 40);
+        employeeList = Arrays.asList(employee01, employee02, employee03, employee04, employee05);
+    }
 
     @Test
     public void shouldAddAnItemSuccessfully(){
@@ -42,24 +53,12 @@ public class CoffeeShopManagementTest {
 
     @Test
     public void shouldAddNewEmployeeSuccessfully(){
-        Employees employee01 = new Employees("Sun Beo", "Supervisor", 02, 30);
-        Employees employee02 = new Employees("Tran Tuan", "Waiter", 01, 20);
-        Employees employee03 = new Employees("Nguyen Minh", "Waiter", 01, 20);
-        Employees employee04 = new Employees("Le Hieu", "Cashier", 03, 30);
-        Employees employee05 = new Employees("Le Hoang", "Warehouse Manager", 03, 40);
-        List<Employees> employeeList = Arrays.asList(employee01,employee02,employee03,employee04,employee05);
         Assert.assertEquals(5, employeeList.stream().count());
         Assert.assertEquals(1, employeeList.stream().filter(as -> "Supervisor".equals(as.getPosition())).count());
     }
 
     @Test
     public void shouldSeeDifferentSalaryForEachLevel(){
-        Employees employee01 = new Employees("Sun Beo", "Supervisor", 1995, 35);
-        Employees employee02 = new Employees("Tran Tuan", "Waiter", 2000, 20);
-        Employees employee03 = new Employees("Nguyen Minh", "Waiter", 1998, 22);
-        Employees employee04 = new Employees("Le Hieu", "Cashier", 1995, 35);
-        Employees employee05 = new Employees("Le Hoang", "Warehouse Manager", 1992, 35);
-        List<Employees> employeeList = Arrays.asList(employee01,employee02,employee03,employee04,employee05);
         Assert.assertEquals(5,employeeList.stream().count());
         Assert.assertEquals(35,employee01.getSalaryOnPosition(),0.00);
         Assert.assertEquals(35,employeeList.get(3).getSalaryOnPosition(),0.00);
